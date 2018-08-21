f2690488f1301d9ed76b99937e521b8894a24100
Sachin Srinivasan <ssampathkumar@qwinix.io>
2018-08-21 23:53:45 +0530

    ghpages

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {/* <input type="file" accept=".txt" onChange={e => handleFileChosen(e.target.file)}/> */}
       </div>
     );
   }
